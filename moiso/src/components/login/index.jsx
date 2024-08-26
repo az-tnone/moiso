87f931f3ad44f2b3f9d2d52fb4d5cecb29e268b8
John Doe <johndoe@example.com>
2024-08-27 02:32:13 +0900

    log in
    log in

--- moiso/src/components/login/index.jsx
+++ moiso/src/components/login/index.jsx
@@ -1,22 +1,48 @@
-import logo from "../../logo.svg";
+import "../../style/Login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const saveData = () => () => {
+    useEffect(() => {
+      if (!localStorage.getItem("isLogin")) {
+        localStorage.setItem("isLogin", true);
+      }
+      onClick("");
+    }, []);
+  };
+  const onClick = (destinationist) => () => {
+    navigate("/" + destinationist);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React a
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="appTitle">
+        즐거운
+        <br />
+        의성으로
+      </h1>
+      <h2 className="appName">모이소</h2>
+
+      <input
+        type="email"
+        placeholder="이메일을 입력하세요."
+        className="inputField"
+      />
+      <input
+        type="password"
+        placeholder="비밀번호를 입력하세요."
+        className="inputField"
+      />
+
+      <button className="submitButton" onClick={saveData()}>
+        로그인
+      </button>
+
+      <div className="divider" />
+
+      <p className="registerText" onClick={onClick("Sign1")}>
+        회원가입
+      </p>
     </div>
   );
 };
